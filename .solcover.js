c5ba8b647dab669a15e586ffcdbeaa8216c81504
TamTamHero <10632523+TamtamHero@users.noreply.github.com>
2022-04-26 15:14:16 +0200

    Remove inadequate solcover skipFiles

--- .solcover.js
+++ .solcover.js
@@ -3,19 +3,9 @@ module.exports = {
   testCommand: 'yarn test',
   compileCommand: 'yarn compile:hardhat',
   skipFiles: [
-    'agToken/polygon',
-    'agToken/AgTokenIntermediateUpgrade.sol',
     'external',
     'interfaces',
     'mock',
-    'oracle/OracleChainlinkMultiTemplate.sol',
-    'oracle/implementations/OracleWSTETHEURChainlink.sol',
-    'oracle/implementations/OracleBTCEURChainlink.sol',
-    'oracle/implementations/OracleETHEURChainlink.sol',
-    // Router here is a copy pasta of the router in another repo
-    'router',
-    'reactor/BaseReactorStorage.sol',
-    'vaultManager/VaultManagerStorage.sol',
   ],
   providerOptions: {
     default_balance_ether: '10000000000000000000000000',
