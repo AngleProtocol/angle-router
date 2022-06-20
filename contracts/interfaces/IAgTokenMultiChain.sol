4e737a618b054d6de3c45f499a6638ceecc50765
Pablo Veyrat <veyratpablo@gmail.com>
2022-06-20 17:38:19 +0200

    getting ready for a multichain router

---
+++ contracts/interfaces/IAgTokenMultiChain.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.12;
+
+/// @title IAgTokenMultiChain
+/// @author Angle Core Team
+/// @notice Interface for the stablecoins `AgToken` contracts in multiple chains
+interface IAgTokenMultiChain {
+
+    function swapIn(
+        address bridgeToken,
+        uint256 amount,
+        address to
+    ) external returns(uint256);
+
+    function swapOut(
+        address bridgeToken,
+        uint256 amount,
+        address to
+    ) external returns (uint256);
+}
