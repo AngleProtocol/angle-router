3ee98e17bcd2ea7f9b29e75a905e355ebf289ea2
Pablo Veyrat <50438397+sogipec@users.noreply.github.com>
2022-11-14 20:05:15 +0100

    upgrade polygon router (#13)
    Co-authored-by: gs8nrv <55771972+GuillaumeNervoXS@users.noreply.github.com>

--- deploy/upgradeRouter.ts
+++ deploy/upgradeRouter.ts
@@ -10,8 +10,8 @@ const func: DeployFunction = async ({ deployments, ethers, network }) => {
   const { deploy } = deployments;
   const [deployer] = await ethers.getSigners();
 
-  const proxyAdminAddress = CONTRACTS_ADDRESSES[ChainId.MAINNET].ProxyAdmin!;
-  const proxyAngleRouterAddress = CONTRACTS_ADDRESSES[ChainId.MAINNET].AngleRouter!;
+  const proxyAdminAddress = CONTRACTS_ADDRESSES[ChainId.POLYGON].ProxyAdmin!;
+  const proxyAngleRouterAddress = CONTRACTS_ADDRESSES[ChainId.POLYGON].AngleRouter!;
 
   console.log('-----------------------------------------------------------------------');
   console.log('Let us get it started with the upgrade of Angle new router');
@@ -20,40 +20,40 @@ const func: DeployFunction = async ({ deployments, ethers, network }) => {
   console.log('');
   console.log('Now deploying AngleRouter');
   console.log('Starting with the implementation');
-  await deploy('AngleRouter_NewImplementation', {
-    contract: 'AngleRouter',
+  await deploy('AngleRouterPolygon_NewImplementation', {
+    contract: 'AngleRouterPolygon',
     from: deployer.address,
     log: !argv.ci,
   });
 
-  const AngleRouterImplementation = (await deployments.get('AngleRouter_NewImplementation')).address;
+  const AngleRouterImplementation = (await deployments.get('AngleRouterPolygon_NewImplementation')).address;
 
   console.log(`Successfully deployed Angle router implementation at the address ${AngleRouterImplementation}`);
   console.log('');
 
-  if (!network.live) {
-    console.log('Now performing the upgrade');
-    const governor = CONTRACTS_ADDRESSES[ChainId.MAINNET].GovernanceMultiSig! as string;
-    await network.provider.request({
-      method: 'hardhat_impersonateAccount',
-      params: [governor],
-    });
-    await network.provider.send('hardhat_setBalance', [governor, '0x10000000000000000000000000000']);
-    const governorSigner = await ethers.provider.getSigner(governor);
+  // if (!network.live) {
+  //   console.log('Now performing the upgrade');
+  //   const governor = CONTRACTS_ADDRESSES[ChainId.MAINNET].GovernanceMultiSig! as string;
+  //   await network.provider.request({
+  //     method: 'hardhat_impersonateAccount',
+  //     params: [governor],
+  //   });
+  //   await network.provider.send('hardhat_setBalance', [governor, '0x10000000000000000000000000000']);
+  //   const governorSigner = await ethers.provider.getSigner(governor);
 
-    console.log('proxyAdmin', proxyAdminAddress);
-    const contractProxyAdmin = new ethers.Contract(
-      proxyAdminAddress,
-      Interfaces.ProxyAdmin_Interface,
-      governorSigner,
-    ) as ProxyAdmin;
-    await (
-      await contractProxyAdmin.connect(governorSigner).upgrade(proxyAngleRouterAddress, AngleRouterImplementation)
-    ).wait();
+  //   console.log('proxyAdmin', proxyAdminAddress);
+  //   const contractProxyAdmin = new ethers.Contract(
+  //     proxyAdminAddress,
+  //     Interfaces.ProxyAdmin_Interface,
+  //     governorSigner,
+  //   ) as ProxyAdmin;
+  //   await (
+  //     await contractProxyAdmin.connect(governorSigner).upgrade(proxyAngleRouterAddress, AngleRouterImplementation)
+  //   ).wait();
 
-    console.log('Success');
-    console.log('');
-  }
+  //   console.log('Success');
+  //   console.log('');
+  // }
 };
 
 func.tags = ['routerUpgrade'];
